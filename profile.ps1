8355846ff6e1c9b5c4763de50dbeab72ab05d0d7
Johnny <nor0x@users.noreply.github.com>
2022-06-01 14:14:31 +0200

    profile.ps1 updated to remember path on duplication / split

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,19 @@
+
+
 Import-Module -Name Terminal-Icons
 
-oh-my-posh --init --shell pwsh --config D:/Dev/WindowsTerminal/ohmyposh.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config D:\WindowsTerminal\ohmyposh.json | Invoke-Expression
+
+#you have to put this in $profile or $profile.currentuserallhosts
+$esc = [char]27
+
+if($env:WT_SESSION){
+	$prevprompt = $Function:prompt
+	function prompt {
+		if ($pwd.provider.name -eq "FileSystem") {
+			$p = $pwd.ProviderPath
+			Write-host "$esc]9;9;`"$p`"$esc\" -NoNewline
+		}
+		return $prevprompt.invoke()
+	}
+}
